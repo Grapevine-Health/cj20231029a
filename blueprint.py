046c1b2c0a96466120dbc73b0113222249ace8da
Charles Jankowski <cjankowski@brain.im>
2023-10-27 09:27:15 -0700

    add blueprint

--- blueprint.py
+++ blueprint.py
@@ -1,14 +1,18 @@
+# Register this blueprint by adding the following line of code 
+# to your entry point file.  
+# app.register_functions(blueprint) 
+# 
+# Please refer to https://aka.ms/azure-functions-python-blueprints
+
+
 import azure.functions as func
 import logging
 
-from blueprint import blueprint
+blueprint = func.Blueprint()
 
-app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
-app.register_functions(blueprint)
-
-@app.route(route="http_trigger")
-def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
+@blueprint.route(route="blueprint, auth_level=func.AuthLevel.FUNCTION")
+def blueprint(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     name = req.params.get('name')
@@ -21,7 +25,7 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed very successfully.")
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
