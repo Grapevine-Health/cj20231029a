a6ae0b9786eddd295694451baff6e6c045574172
Charles Jankowski <cjankowski@brain.im>
2023-10-30 16:11:27 -0700

    Change name of blueprint function, avoid conflict

--- blueprint.py
+++ blueprint.py
@@ -12,7 +12,7 @@ blueprint = func.Blueprint()
 
 
 @blueprint.route(route="blueprint, auth_level=func.AuthLevel.FUNCTION")
-def blueprint(req: func.HttpRequest) -> func.HttpResponse:
+def http_trigger2(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     name = req.params.get('name')
